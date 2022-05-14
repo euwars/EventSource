b5c8ab7692fbc1b7b45f9bb0eff81d2c5b5e50bb
Farzad <nazifi10@gmail.com>
2022-05-14 22:51:43 +0200

    +

--- Package.swift
+++ Package.swift
@@ -1,30 +1,32 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "EventSource",
-	platforms: [
-		.iOS("8.0"),
-		.macOS("10.10"),
-	],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "EventSource",
-            targets: ["EventSource"]),
-    ],
-    dependencies: [],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "EventSource",
-			dependencies: [],
-			path: "EventSource"),
-        .testTarget(
-			name: "EventSourceTests",
-			dependencies: ["EventSource"],
-			path: "EventSourceTests"),
-    ]
+  name: "EventSource",
+  platforms: [
+    .macOS(.v11),
+    .iOS(.v14),
+    .tvOS(.v14),
+    .watchOS(.v7)
+  ],
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "EventSource",
+      targets: ["EventSource"]),
+  ],
+  dependencies: [],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+    .target(
+      name: "EventSource",
+      dependencies: [],
+      path: "EventSource"),
+    .testTarget(
+      name: "EventSourceTests",
+      dependencies: ["EventSource"],
+      path: "EventSourceTests"),
+  ]
 )
